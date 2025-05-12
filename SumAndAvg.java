0040ac32b42c547301a9395893f1bf695c12e1c1
Harsha Damarla <95854277+HarshaDamarla@users.noreply.github.com>
2025-05-13 00:13:43 +1000

    Update SumAndAvg.java

--- SumAndAvg.java
+++ SumAndAvg.java
@@ -1,34 +1,17 @@
-package com.arrays.programs;
-
-import java.util.Scanner;
-
 public class SumAndAvg {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50};
+        int sum = 0;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int sum=0;
-		float avg;
-		Scanner s = new Scanner(System.in);
-		System.out.println("Enter the size of an array: ");
-		int size = s.nextInt();
-		int a[] = new int[size];
-		System.out.println("Enter the array values: ");
-		
-		for(int i=0; i<a.length; i++)
-		{
-			a[i] = s.nextInt();
-		}
-		//Sum of array
-		for(int j=0; j<a.length; j++)
-		{
-			sum=sum+a[j];
-		}
-		System.out.println("Sum of an array is: "+sum);
-		
-		//Average of an array
-		avg=(float)sum/a.length;
-		System.out.println("Average of an array is: "+ avg);
+        System.out.println("Input numbers:");
+        for (int num : numbers) {
+            System.out.println(num);
+            sum += num;
+        }
 
-	}
+        double average = (double) sum / numbers.length;
 
+        System.out.println("Sum = " + sum);
+        System.out.println("Average = " + average);
+    }
 }
